4d19c783aaeb860a256e04ee17f6e0b69057df4d
meerim05 <meerimt005@gmail.com>
2021-06-15 17:41:38 -0400

    Features of sun are added but there is an error

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -7,5 +7,11 @@ public class Main {
 	FactorX.Sun=1;
 	FactorX.Moon=1;
 	FactorX.Planets=2;
+
+        //working on sun features
+        Sun Feron = new Sun();
+        Feron.Heat="46948214";
+        Feron.Radius="34024209423";
+        Feron.Temperature="1000000000000";
     }
 }
